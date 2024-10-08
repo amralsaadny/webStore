47f93abededc28291e16e2eaff4796e3736ed3f4
amr <amralsaadny2@gmail.com>
2024-10-08 15:27:11 +0300

    making custmer table with its attributes

--- src/main/java/com/example/demo/entities/Customers.java
+++ src/main/java/com/example/demo/entities/Customers.java
@@ -1,4 +1,25 @@
 package com.example.demo.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Customers {
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private String customerID;
+    private String userName;
+    private String email;
+    private String password;
+
 }
